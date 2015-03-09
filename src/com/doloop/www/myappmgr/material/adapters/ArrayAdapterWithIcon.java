4356b8889f491455e47159901eb8672f96eda730
MrDoLoop <nilaile8@gmail.com>
2015-03-09 10:11:33 +0800

    ttt

--- src/com/doloop/www/myappmgr/material/adapters/ArrayAdapterWithIcon.java
+++ src/com/doloop/www/myappmgr/material/adapters/ArrayAdapterWithIcon.java
@@ -1,5 +1,7 @@
 package com.doloop.www.myappmgr.material.adapters;
 
+import com.doloop.www.myappmgrmaterial.R;
+
 import android.content.Context;
 import android.util.TypedValue;
 import android.view.View;
@@ -10,11 +12,13 @@ import android.widget.TextView;
 public class ArrayAdapterWithIcon extends ArrayAdapter<String> {
 	//private String[] OptionStr;
 	private int[] OptionIcon;
+	private Context mCtx;
 	
 	
 	public ArrayAdapterWithIcon(Context context, String[] optionStr, int[] optionIcon) {
 	    super(context, android.R.layout.simple_list_item_1, optionStr);
 	    //OptionStr = optionStr;
+	    mCtx = context;
 	    OptionIcon = optionIcon;
 	}
 
@@ -22,6 +26,7 @@ public class ArrayAdapterWithIcon extends ArrayAdapter<String> {
 	public View getView(int position, View convertView, ViewGroup parent) {
 	    View view = super.getView(position, convertView, parent);
 	    TextView textView = (TextView) view.findViewById(android.R.id.text1);
+	    textView.setTextColor(mCtx.getResources().getColor(R.color.black));
 	    textView.setCompoundDrawablesWithIntrinsicBounds(OptionIcon[position], 0, 0, 0);
 	    textView.setCompoundDrawablePadding(
 	            (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 6, getContext().getResources().getDisplayMetrics()));
