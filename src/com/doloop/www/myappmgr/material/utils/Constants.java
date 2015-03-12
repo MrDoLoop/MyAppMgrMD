418b0e900bde2ce71e78e95418d679d06374ef77
MrDoLoop <nilaile8@gmail.com>
2015-03-12 10:45:24 +0800

    ttt

--- src/com/doloop/www/myappmgr/material/utils/Constants.java
+++ src/com/doloop/www/myappmgr/material/utils/Constants.java
@@ -5,7 +5,7 @@ public class Constants {
     public final static int SYS_APPS_TAB_POS = 1;
     public final static int BACKUP_APPS_TAB_POS = 2;
     
-    public static final boolean SAVE_APP_ICON_IN_OBJ = true;
+    public static final boolean SAVE_APP_ICON_IN_OBJ = false;
     
     public static String MY_PACKAGE_NAME = "";
     
