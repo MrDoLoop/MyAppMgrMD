5d5d3f8dcf5488073dd7896ae24cdb76d51de418
MrDoLoop <nilaile8@gmail.com>
2015-03-12 10:37:30 +0800

    简单的header adapter

--- src/com/doloop/www/myappmgr/material/adapters/BackupAppListAdapter.java
+++ src/com/doloop/www/myappmgr/material/adapters/BackupAppListAdapter.java
@@ -2,7 +2,6 @@ package com.doloop.www.myappmgr.material.adapters;
 
 import java.io.File;
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Locale;
 
 import org.apache.commons.io.FileUtils;
@@ -24,13 +23,14 @@ import com.doloop.www.myappmgr.material.MainActivity;
 import com.doloop.www.myappmgr.material.dao.AppInfo;
 import com.doloop.www.myappmgr.material.utils.Constants;
 import com.doloop.www.myappmgr.material.utils.Utils;
-import com.doloop.www.myappmgr.material.widgets.ParallaxRecyclerAdapter;
 import com.doloop.www.myappmgrmaterial.R;
 import com.squareup.picasso.Picasso;
 import com.squareup.picasso.Target;
 import com.squareup.picasso.Picasso.LoadedFrom;
 
-public class BackupAppListAdapter extends ParallaxRecyclerAdapter<AppInfo> {
+public class BackupAppListAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+    private static final int TYPE_HEADER = 0;
+    private static final int TYPE_ITEM = 1;
 
     public BackupAppListDataSetChangedListener mBackupAppListDataSetChangedListener;
 
@@ -68,92 +68,113 @@ public class BackupAppListAdapter extends ParallaxRecyclerAdapter<AppInfo> {
     }
 
     public BackupAppListAdapter(Context ctx, ArrayList<AppInfo> appList) {
-        super(appList);
+        appList.add(0, new AppInfo());
         mAppListFull = mAppListDisplay = appList;
         mCtx = ctx;
-        super.implementRecyclerAdapterMethods(new ParallaxRecyclerAdapter.RecyclerAdapterMethods() {
-
-            @Override
-            public void onBindViewHolder(android.support.v7.widget.RecyclerView.ViewHolder viewHolder, int position) {
-                // TODO Auto-generated method stub
-                AppInfo appInfo = mAppListDisplay.get(position);
-                ItemViewHolder holder = (ItemViewHolder) viewHolder;
-                holder.AppNameTextView.setText(appInfo.appName);
-                // holder.AppIconImageView.setImageBitmap(appInfo.iconBitmap);
-                holder.AppIconImageView.setTag(position);
-                holder.AppVersionTextView.setText("v" + appInfo.versionName + " | " + appInfo.appSizeStr + " | "
-                        + appInfo.lastModifiedTimeStr);
-                holder.AppFileNameTextView.setText(appInfo.getBackupApkFileName());
-                holder.RootLayout.setTag(appInfo);
-
-                if (appInfo.iconBitmap == null) {
-                    Picasso.with(mCtx).load(appInfo.getAppIconCachePath(mCtx)).noFade().into(holder);
-                } else {
-                    holder.AppIconImageView.setImageBitmap(appInfo.iconBitmap);
-                }
-            }
-
-            @Override
-            public android.support.v7.widget.RecyclerView.ViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
-                // TODO Auto-generated method stub
-                View v = null;
-                ViewHolder holder = null;
-                v = LayoutInflater.from(mCtx).inflate(R.layout.back_app_info_item, viewGroup, false);
-                holder = new ItemViewHolder(v);
-                return holder;
-            }
-
-            @Override
-            public int getItemCount() {
-                // TODO Auto-generated method stub
-                return mAppListDisplay.size();
-            }
-        });
     }
 
     public ArrayList<AppInfo> getDisplayList() {
         return mAppListDisplay;
     }
 
-    /*public AppInfo getItem(int position) {
+    public AppInfo getItem(int position) {
         return mAppListDisplay.get(position);
-    }*/
-    
-    public int getAppItemCount(){
+    }
+
+    @Override
+    public int getItemCount() {
+        // TODO Auto-generated method stub
         return mAppListDisplay.size();
     }
 
-    /*
-     * @Override public int getItemCount() { // TODO Auto-generated method stub return mAppListDisplay.size(); }
-     */
-    /*
-     * @Override public void onBindViewHolder(ViewHolder viewHolder, int position) { // TODO Auto-generated method stub
-     * AppInfo appInfo = mAppListDisplay.get(position); ItemViewHolder holder = (ItemViewHolder) viewHolder;
-     * holder.AppNameTextView.setText(appInfo.appName); //holder.AppIconImageView.setImageBitmap(appInfo.iconBitmap);
-     * holder.AppIconImageView.setTag(position); holder.AppVersionTextView.setText("v" + appInfo.versionName + " | " +
-     * appInfo.appSizeStr + " | " + appInfo.lastModifiedTimeStr);
-     * holder.AppFileNameTextView.setText(appInfo.getBackupApkFileName()); holder.RootLayout.setTag(appInfo);
-     * 
-     * 
-     * if (appInfo.iconBitmap == null) {
-     * Picasso.with(mCtx).load(appInfo.getAppIconCachePath(mCtx)).noFade().into(holder); } else {
-     * holder.AppIconImageView.setImageBitmap(appInfo.iconBitmap); }
-     * 
-     * }
-     */
-    /*
-     * @Override public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) { // TODO Auto-generated method
-     * stub View v = null; ViewHolder holder = null; v =
-     * LayoutInflater.from(parent.getContext()).inflate(R.layout.back_app_info_item, parent, false); holder = new
-     * ItemViewHolder(v); return holder; }
-     */
+    @Override
+    public void onBindViewHolder(ViewHolder viewHolder, int position) {
+        // TODO Auto-generated method stub
+        if (getItemViewType(position) == TYPE_HEADER) {
+            HeaderViewHolder holder = (HeaderViewHolder) viewHolder;
+            // holder.AppFileNameTextView.setText("赵楠");
+        } else if (getItemViewType(position) == TYPE_ITEM) {
+            AppInfo appInfo = mAppListDisplay.get(position);
+            ItemViewHolder holder = (ItemViewHolder) viewHolder;
+            holder.AppNameTextView.setText(appInfo.appName);
+            // holder.AppIconImageView.setImageBitmap(appInfo.iconBitmap);
+            holder.AppIconImageView.setTag(position);
+            holder.AppVersionTextView.setText("v" + appInfo.versionName + " | " + appInfo.appSizeStr + " | "
+                    + appInfo.lastModifiedTimeStr);
+            holder.AppFileNameTextView.setText(appInfo.getBackupApkFileName());
+            holder.RootLayout.setTag(appInfo);
+
+            if (appInfo.iconBitmap == null) {
+                Picasso.with(mCtx).load(appInfo.getAppIconCachePath(mCtx)).noFade().into(holder);
+            } else {
+                holder.AppIconImageView.setImageBitmap(appInfo.iconBitmap);
+            }
+        }
+    }
 
     @Override
-    public void setData(List<AppInfo> data) {
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         // TODO Auto-generated method stub
-        mAppListFull = mAppListDisplay = (ArrayList<AppInfo>) data;
-        super.setData(data);
+        Context context = parent.getContext();
+        if (viewType == TYPE_HEADER) {
+            final View view = LayoutInflater.from(context).inflate(R.layout.backup_list_header, parent, false);
+            return new HeaderViewHolder(view);
+        } else if (viewType == TYPE_ITEM) {
+            final View view = LayoutInflater.from(context).inflate(R.layout.back_app_info_item, parent, false);
+            return new ItemViewHolder(view);
+        }
+        throw new RuntimeException("There is no type that matches the type " + viewType
+                + " + make sure your using types correctly");
+    }
 
+    @Override
+    public int getItemViewType(int position) {
+        if (position == 0) {
+            return TYPE_HEADER;
+        } else {
+            return TYPE_ITEM;
+        }
+
+    }
+
+    public class HeaderViewHolder extends RecyclerView.ViewHolder {
+
+        TextView AppNameTextView;
+        TextView AppVersionTextView;
+        TextView AppFileNameTextView;
+        ImageView AppIconImageView;
+        RelativeLayout RootLayout;
+
+        public HeaderViewHolder(View view) {
+            super(view);
+            AppNameTextView = (TextView) view.findViewById(R.id.textView1);
+            /*
+             * AppNameTextView = (TextView) view.findViewById(R.id.app_name); AppVersionTextView = (TextView)
+             * view.findViewById(R.id.app_version); AppFileNameTextView = (TextView)
+             * view.findViewById(R.id.app_filename); AppIconImageView = (ImageView) view.findViewById(R.id.app_icon);
+             * AppIconImageView.setOnClickListener(new View.OnClickListener() {
+             * 
+             * @Override public void onClick(View v) { // TODO Auto-generated method stub
+             * //MainActivity.T("Backup图标被点击了: "+AppIconImageView.getTag()); mAppListDisplay.remove(getPosition());
+             * notifyDataSetChanged(); AppInfo appInfo = null; try{//因为添加了动画，点击过快的话，删除动画还没有完成，此处就崩溃了 appInfo =
+             * mAppListDisplay.get(getPosition()); }catch(Exception e){ return ; }
+             * 
+             * 
+             * //if(FileUtils.deleteQuietly(new File(appInfo.apkFilePath))){ if(FileUtils.deleteQuietly(new
+             * File(appInfo.backupFilePath))){ //从显示的list中删除 mAppListDisplay.remove(getPosition()); //从总共的list中删除
+             * for(int i =0;i<mAppListFull.size();i++){ if(mAppListFull.get(i).packageName.equals(appInfo.packageName)){
+             * mAppListFull.remove(i); break; } }
+             * 
+             * notifyItemRemoved(getPosition());
+             * //getBackupAppListDataSetChangedListener().OnBackupAppListDataSetChanged(); } else{
+             * MainActivity.T(R.string.error); } } }); RootLayout = (RelativeLayout) view.findViewById(R.id.rootLayout);
+             * RootLayout.setOnClickListener(new View.OnClickListener() {
+             * 
+             * @Override public void onClick(View v) { // TODO Auto-generated method stub AppInfo theApp =
+             * mAppListDisplay.get(getPosition()); if(TextUtils.isEmpty(theApp.backupFilePath)){ Utils.installAPK(mCtx,
+             * theApp.apkFilePath); } else{ Utils.installAPK(mCtx, theApp.backupFilePath); } } });
+             */
+        }
     }
 
     public class ItemViewHolder extends RecyclerView.ViewHolder implements Target {
@@ -207,16 +228,15 @@ public class BackupAppListAdapter extends ParallaxRecyclerAdapter<AppInfo> {
                      */
                     AppInfo appInfo = null;
                     try {// 因为添加了动画，点击过快的话，删除动画还没有完成，此处就崩溃了
-                        appInfo = mAppListDisplay.get(getPosition() - (hasHeader() ? 1 : 0));
+                        appInfo = mAppListDisplay.get(getPosition());
                     } catch (Exception e) {
                         return;
                     }
 
                     // if(FileUtils.deleteQuietly(new File(appInfo.apkFilePath))){
                     if (FileUtils.deleteQuietly(new File(appInfo.backupFilePath))) {
-                        
                         // 从显示的list中删除
-                        mAppListDisplay.remove(getPosition() - (hasHeader() ? 1 : 0));
+                        mAppListDisplay.remove(getPosition());
                         // 从总共的list中删除
                         for (int i = 0; i < mAppListFull.size(); i++) {
                             if (mAppListFull.get(i).packageName.equals(appInfo.packageName)) {
@@ -238,7 +258,7 @@ public class BackupAppListAdapter extends ParallaxRecyclerAdapter<AppInfo> {
                 @Override
                 public void onClick(View v) {
                     // TODO Auto-generated method stub
-                    AppInfo theApp = mAppListDisplay.get(getPosition() - (hasHeader() ? 1 : 0));
+                    AppInfo theApp = mAppListDisplay.get(getPosition());
                     if (TextUtils.isEmpty(theApp.backupFilePath)) {
                         Utils.installAPK(mCtx, theApp.apkFilePath);
                     } else {
