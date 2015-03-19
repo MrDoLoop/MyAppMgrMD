47333845839ae7f8855f20f3d269c73c689094fd
MrDoLoop <nilaile8@gmail.com>
2015-03-19 23:53:36 +0800

    ttt

--- src/com/doloop/www/mayappmgr/material/events/ActionModeToggleEvent.java
+++
@@ -1,8 +0,0 @@
-package com.doloop.www.mayappmgr.material.events;
-
-public class ActionModeToggleEvent {
-    public boolean isInActionMode = false;
-    public ActionModeToggleEvent(boolean isActionMode){
-        this.isInActionMode = isActionMode;
-    }
-}
--- src/com/doloop/www/mayappmgr/material/events/ViewNewBackupAppEvent.java
+++
@@ -1,7 +0,0 @@
-package com.doloop.www.mayappmgr.material.events;
-
-public class ViewNewBackupAppEvent {
-    public ViewNewBackupAppEvent(){
-        
-    }
-}
