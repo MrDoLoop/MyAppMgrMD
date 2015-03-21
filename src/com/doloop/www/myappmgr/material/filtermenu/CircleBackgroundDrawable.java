77243f00b0aafc107a46dbf60f5d78dc934c89b9
MrDoLoop <nilaile8@gmail.com>
2015-03-22 00:26:35 +0800

    ttt

---
+++ src/com/doloop/www/myappmgr/material/filtermenu/CircleBackgroundDrawable.java
@@ -0,0 +1,30 @@
+package com.doloop.www.myappmgr.material.filtermenu;
+
+import android.graphics.Canvas;
+import android.graphics.ColorFilter;
+import android.graphics.drawable.Drawable;
+
+/**
+ * Created by linroid on 15/3/11.
+ */
+public class CircleBackgroundDrawable extends Drawable{
+    @Override
+    public void draw(Canvas canvas) {
+
+    }
+
+    @Override
+    public void setAlpha(int alpha) {
+
+    }
+
+    @Override
+    public void setColorFilter(ColorFilter cf) {
+
+    }
+
+    @Override
+    public int getOpacity() {
+        return 0;
+    }
+}
