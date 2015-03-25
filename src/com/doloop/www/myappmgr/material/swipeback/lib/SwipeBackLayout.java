736443029fe20bab2132f4745711f10c637ca13f
MrDoLoop <nilaile8@gmail.com>
2015-03-25 13:13:55 +0800

    ttt

--- src/com/doloop/www/myappmgr/material/swipeback/lib/SwipeBackLayout.java
+++ src/com/doloop/www/myappmgr/material/swipeback/lib/SwipeBackLayout.java
@@ -542,6 +542,7 @@ public class SwipeBackLayout extends FrameLayout {
             if (mScrollPercent >= 1) {
                 if (!mActivity.isFinishing())
                     mActivity.finish();
+                    mActivity.overridePendingTransition(0,0);
             }
         }
 
