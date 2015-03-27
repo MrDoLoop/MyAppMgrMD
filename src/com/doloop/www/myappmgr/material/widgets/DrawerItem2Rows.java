030b8f41411decf485f23764b7e0254e2eeb791e
MrDoLoop <nilaile8@gmail.com>
2015-03-27 16:19:01 +0800

    ttt

--- src/com/doloop/www/myappmgr/material/widgets/DrawerItem2Rows.java
+++ src/com/doloop/www/myappmgr/material/widgets/DrawerItem2Rows.java
@@ -6,7 +6,6 @@ import android.content.res.TypedArray;
 import android.os.Build;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
-import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
