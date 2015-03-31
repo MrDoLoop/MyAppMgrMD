349aa50312200afbd8e2c707409f00ccd0c1504d
MrDoLoop <nilaile8@gmail.com>
2015-03-31 14:29:59 +0800

    ttt

---
+++ src/com/doloop/www/myappmgr/material/interfaces/IhoverMenuClickListener.java
@@ -0,0 +1,7 @@
+package com.doloop.www.myappmgr.material.interfaces;
+
+import com.doloop.www.myappmgr.material.dao.AppInfo;
+
+public interface IhoverMenuClickListener {
+    public void OnMenuClick(int viewId, int listPos, AppInfo appInfo);
+}
