e2d5197b303fd6c4d0b009a9a9502eaeb69c0557
MrDoLoop <nilaile8@gmail.com>
2015-04-06 18:56:30 +0800

    ttt

---
+++ src/com/doloop/www/myappmgr/material/filtermenu/FilterMenuItemWapper.java
@@ -0,0 +1,12 @@
+package com.doloop.www.myappmgr.material.filtermenu;
+
+public class FilterMenuItemWapper {
+    public int IconResId = -1;
+    public String MenuTag = "";
+    
+    
+    public FilterMenuItemWapper(int iconResId, String menuTag){
+        IconResId = iconResId;
+        MenuTag = menuTag;
+    }
+}
