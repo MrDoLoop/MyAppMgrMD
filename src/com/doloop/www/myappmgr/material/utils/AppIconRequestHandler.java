b577f282d415e0a44ff68e25bd779d0d375ebd68
MrDoLoop <nilaile8@gmail.com>
2015-04-18 20:43:43 +0800

    ttt

--- src/com/doloop/www/myappmgr/material/utils/AppIconRequestHandler.java
+++ src/com/doloop/www/myappmgr/material/utils/AppIconRequestHandler.java
@@ -2,6 +2,8 @@ package com.doloop.www.myappmgr.material.utils;
 import java.io.IOException;
 
 import android.content.Context;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.drawable.Drawable;
@@ -14,6 +16,7 @@ public class AppIconRequestHandler extends RequestHandler {
 
     /** Uri scheme for app icons */
     public static final String SCHEME_APP_ICON = "app-icon";
+    public static final String SCHEME_BACKUP_APP_ICON = "backup-app-icon";
 
     private PackageManager mPackageManager;
 
@@ -24,7 +27,7 @@ public class AppIconRequestHandler extends RequestHandler {
     @Override
     public boolean canHandleRequest(Request data) {
         // only handle Uris matching our scheme
-        return (SCHEME_APP_ICON.equals(data.uri.getScheme()));
+        return (SCHEME_APP_ICON.equals(data.uri.getScheme()) || SCHEME_BACKUP_APP_ICON.equals(data.uri.getScheme()));
     }
 
     @Override
@@ -32,9 +35,23 @@ public class AppIconRequestHandler extends RequestHandler {
 
         Drawable drawable = null;
         Bitmap bitmap = null;
+        String schemeStr = request.uri.getScheme();
         try {
-            drawable = mPackageManager.getApplicationIcon(request.uri.toString()
-                            .replace(SCHEME_APP_ICON + ":", ""));
+            if(SCHEME_APP_ICON.equals(schemeStr)){
+                String appPkgName = request.uri.toString().replace(SCHEME_APP_ICON + ":", "");
+                drawable = mPackageManager.getApplicationIcon(appPkgName);
+            }
+            else if(SCHEME_BACKUP_APP_ICON.equals(schemeStr)){
+                String apkFilePath = request.uri.toString().replace(SCHEME_BACKUP_APP_ICON + ":", "");
+                PackageInfo packageInfo = mPackageManager.getPackageArchiveInfo(apkFilePath, PackageManager.GET_ACTIVITIES);
+                if (packageInfo != null) {
+                    ApplicationInfo applicationInfo = packageInfo.applicationInfo;
+                    //以下2个属性赋值之后才能正确读出东西
+                    applicationInfo.sourceDir = apkFilePath;
+                    applicationInfo.publicSourceDir = apkFilePath;
+                }
+                drawable = packageInfo.applicationInfo.loadIcon(mPackageManager);
+            }
         } catch (PackageManager.NameNotFoundException ignored) {
             ignored.printStackTrace();
         }
